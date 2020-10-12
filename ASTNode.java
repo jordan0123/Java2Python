e1751ca4c4979c2129bbb2be0ce91fcc45b7a341
Patrick Sweeney <plsweeney25@gmail.com>
2020-10-12 11:40:56 -0400

    Added key and depth properties to ASTNode

--- ASTNode.java
+++ ASTNode.java
@@ -1,16 +1,22 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 class ASTNode {
     private ASTNode parent;
     private ArrayList<ASTNode> children;
     private String type;
     private String value;
-     
+    private String key;
+    private int depth;
+    
     ASTNode(String type, String value) 
     {
         this.type = type;
         this.value = value;
+        this.key = null;
+        this.depth = -1;
         children = new ArrayList<ASTNode>();
+        genKey();
     }
     // add child node
     void addChild(ASTNode child) 
@@ -28,5 +34,29 @@ class ASTNode {
     // get parent node
     ASTNode getParent() { return parent; }
     
+    
+    //generate key (for identifying objects in a list/maintaining uniqueness)
+    void genKey(){
+        Random rand = new Random();
+        int key_length = 16;
+        String key_alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder sb = new StringBuilder(key_length);
+        for(int i = 0; i < key_length; i++){
+            int j = rand.nextInt(key_alpha.length());
+            char c = key_alpha.charAt(j);
+            sb.append(c);
+        }
+        this.key = sb.toString();
+    }
+    String getKey(){
+        return this.key;
+    }
+    // for printing depth
+    void setDepth(int n){
+        this.depth = n;
+    }
+    int getDepth(){
+        return this.depth;
+    }
     
 }
