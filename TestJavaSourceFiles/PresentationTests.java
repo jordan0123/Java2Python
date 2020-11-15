7b3832273b96d375fc149d8e247f24fed0066cac
Christian <christianbyrne2012@gmail.com>
2020-11-15 18:30:28 -0500

    Enclose SwitchTest.java in a class definition and a main 'driver' test for our presentation.

---
+++ TestJavaSourceFiles/PresentationTests.java
@@ -0,0 +1,9 @@
+class PresentationTests {
+    public static void main(String[] args) {
+        SwitchTest switchTest;
+        
+        System.out.println("*** SwitchTest START ***");
+        for (int i = 1; i < 11; i++) switchTest = new SwitchTest(i);
+        System.out.println("*** SwitchTest  END  ***");
+    }
+}
--- TestJavaSourceFiles/SwitchTest.java
+++ TestJavaSourceFiles/SwitchTest.java
@@ -1,44 +1,53 @@
-int a = 1;
-int b = 2; // do not change
-int c = 3; // do not change
+class SwitchTest {
+    public SwitchTest(int a) {
+        int b = 2; // do not change
+        int c = 3; // do not change
 
-switch (a) {
-    case 1:
-    b = 5;
-    case 2:
-    case 3:
-    c = 10;
-    if (b == 5) { break; }
+        System.out.println("a = " + a);
 
-    case 9:
-    b = 2;
-    switch (c) {
-        case 10:
-        a += 3; // a should be 5 or 6 here
-        if (a == 6) { break; }
-        b = 1;
-        break;
-        case 20:
-        if (a == 5) {
+        switch (a) {
+            case 1:
             b = 5;
-            c = 70;
-        } else if (a == 6) {
-            b = 20;
-            c = 30;
+            case 2:
+            b = b;
+            case 3:
+            c = 10;
+            if (b == 5) { break; }
+
+            case 9:
+            b = 2;
+            switch (c) {
+                case 10:
+                a += 3; // a should be 5 or 6 here
+                if (a == 6) { break; }
+                b = 1;
+                break;
+                case 20:
+                if (a == 5) {
+                    b = 5;
+                    c = 70;
+                    while (true) {
+                        break;
+                    }
+                } else if (a == 6) {
+                    b = 20;
+                    c = 30;
+                    break;
+                } else {
+                    c = 22;
+                }
+
+                b = 10;
+            }
             break;
-        } else {
-            c = 22;
+
+            default:
+            a = 7; b = 7; c = 7;
         }
 
-        b = 10;
+        System.out.println(a + ", " + b + ", " + c);
+        // a = 1 (1, 5, 10)
+        // a = 2 (5, 2, 10)
+        // a = 3 (6, 1, 10)
     }
-    break;
-
-    default:
-    a = 7; b = 7; c = 7;
 }
-
-System.out.println(a + ", " + b + ", " + c);
-// a = 1 (1, 5, 10)
-// a = 2 (5, 2, 10)
-// a = 3 (6, 1, 10)
